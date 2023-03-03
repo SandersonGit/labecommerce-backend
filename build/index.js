0bd766a368b0970bb13a5b1bf5624224979082ac
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2023-03-02 21:11:25 -0300

    Typando_ex1_ex2_ex3

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = require("./database");
+console.log(database_1.users);
+console.log(database_1.products);
+console.log(database_1.purchases);
+//# sourceMappingURL=index.js.map
