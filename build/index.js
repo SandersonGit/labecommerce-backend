ad70523466a24d62ca3dc048860ed0009d5edcbf
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2023-03-14 20:52:21 -0300

    ts_ex_002

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const database_1 = require("./database");
-console.log(database_1.users);
-console.log(database_1.products);
-console.log(database_1.purchases);
-//# sourceMappingURL=index.js.map
