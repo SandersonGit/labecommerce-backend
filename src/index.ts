1361a6b6edf50ca98d7550c35cfceb25dff28576
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2023-04-16 21:10:31 -0300

    introdução sql

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { users, products, purchases } from "./database";
+
 import express, { Request, Response } from "express";
 import cors from "cors";
-import { user, product, purchase  } from "./types";
+import { Cavaleiro } from "./types";
 
-console.log(users);
-console.log(products);
-console.log(purchases);
+// console.log(users);
+// console.log(products);
+// console.log(purchases);
 
 const app = express();
 
@@ -20,61 +20,4 @@ app.listen(3003, () => {
 app.get("/ping", (req: Request, res: Response) => {
   res.send("Pong!");
 });
-app.get("/users", (req: Request, res: Response) => {
-  res.status(200).send(users);
-});
-
-app.get("/products", (req: Request, res: Response) => {
-  res.status(200).send(products);
-});
-
-app.get("/products/search", (req: Request, res: Response) => {
-  const q = req.query.name as string;
-
-  const result = products.filter((product) => {
-    return product.name.toLowerCase().includes(q.toLowerCase());
-  });
-  res.status(200).send(result);
-});
-
-app.post("/user", (req: Request, res: Response) => {
-  const { id, email, password } = req.body;
-
-  const newUsers: user = {
-    id,
-    email,
-    password
-  };
-  users.push(newUsers)
-  res.status(201).send("Cadastro realizado com sucesso!");
-});
-
-app.post('/product', (req: Request, res: Response)=>{
-    const { id, name, price, category } = req.body;
-
-    const newProduct: product = {
-        id,
-        name,
-        price,
-        category
-    }
-    products.push(newProduct);
-    res.status(201).send("Produto cadastrado com sucesso!");
-})
-
-app.post('/purchase', (req: Request, res: Response)=>{
-    const { userId, productId, quantity, totalPrice } = req.body
-
-    const newPurchase: purchase = {
-        userId,
-        productId,
-        quantity,
-        totalPrice
-
-    }
-    purchases.push(newPurchase);
-    res.status(201).send("Compra realizada com sucesso!")
-
-})
-
 
